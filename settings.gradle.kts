a8368f919f133b443b76cbf5e87b94eab1ab9f5d
Mina Pattanaik <mpatt8@uic.edu>
2026-02-22 14:47:24 -0600

    implemented simple pipeline to test app functionality

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,10 +16,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io")
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
-}
 
-rootProject.name = "CS 423 Application"
-include(":app")
- 
+    rootProject.name = "CS 423 Application"
+    include(":app")
+}
