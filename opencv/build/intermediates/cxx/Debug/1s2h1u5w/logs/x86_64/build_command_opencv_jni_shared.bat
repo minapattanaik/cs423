466c29f62c9d492138f247cc303228a999f6d11a
Mina Pattanaik <mpatt8@uic.edu>
2026-03-11 18:14:34 -0500

    remove tracked build artifacts & large sdks

--- opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Huda\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Huda\\Documents\\CS478\\cs423\\opencv\\.cxx\\Debug\\1s2h1u5w\\x86_64" ^
-  opencv_jni_shared
